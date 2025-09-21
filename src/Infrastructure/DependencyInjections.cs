3cfe9c91142f1b920aa1440d1d9797bff4188af3
Marcos Lima <marcosviniciuslima20@hotmail.com>
2025-09-21 17:03:48 -0300

    chore: inject dbcontext

---
+++ src/Infrastructure/DependencyInjections.cs
@@ -0,0 +1,32 @@
+﻿using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+
+namespace Infrastructure;
+
+public static class DependencyInjections
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+
+        services.AddDbContext<MedSchedDbContext>((sp,options) =>
+            options.UseNpgsql
+            (
+                sp.GetRequiredService<IConfiguration>().BuildConnectionString(nameof(MedSchedDbContext)),
+                sqlOptions => sqlOptions.MigrationsAssembly(typeof(MedSchedDbContext).Assembly.FullName))
+            );
+        return services;
+    }
+    private static string BuildConnectionString(this IConfiguration configuration, string name)
+    {
+        var username = configuration.GetValue<string>("DB_USERNAME") ?? throw new InvalidOperationException("Database username not found.");
+        var password = configuration.GetValue<string>("DB_PASSWORD") ?? throw new InvalidOperationException("Database password not found.");
+        var connectionString = configuration.GetConnectionString(name) ?? throw new InvalidOperationException($"Connection string '{name}' not found.");
+        var sb = new StringBuilder(connectionString);
+        sb.Append($"Username={username};Password={password}");
+        return sb.ToString();
+    }
+       
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,8 +1,10 @@
 using Scalar.AspNetCore;
-
+using Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+builder.Services
+    .AddOpenApi()
+    .AddInfrastructure();
 
 var app = builder.Build();
 
